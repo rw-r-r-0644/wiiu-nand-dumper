a154a440e685a3dc080310b84a29a0d377cb8fb7
Ash <quarktheawesome@gmail.com>
2018-03-04 12:51:04 +1100

    CFG: Merge boot.cfg add, allows custom commandlines + kernel locations
    Squashed commit of the following:
    
    commit 5b082f441f730e238007d5a4d44c72a0f988d0a8
    Author: Ash <quarktheawesome@gmail.com>
    Date:   Sun Mar 4 12:49:34 2018 +1100
    
        README: Remove unnecessary "more on this later"
    
    commit 39fdcdefacb389578ca766d3e8dd7fd4700f22b9
    Author: Ash <quarktheawesome@gmail.com>
    Date:   Sun Mar 4 12:44:12 2018 +1100
    
        README: Add boot.cfg example and instructions
    
    commit 664210a19698f7d1db0b40f7bad3412d88612716
    Author: Ash <quarktheawesome@gmail.com>
    Date:   Wed Feb 28 17:19:40 2018 +1100
    
        [cfg] Add boot configuration for custom commandlines and other stuff

--- README.md
+++ README.md
@@ -32,3 +32,20 @@ You'll need an SD card normally readable by the Wii U. Simply copy the fw.img to
 
 ### Running
 Once you've done everything in the Setup stage, just run your favorite CFW booter (CBHC works, as does the old wiiubru loader). With any luck, linux-loader will start and boot up the Linux kernel.
+
+### Advanced Setup (boot.cfg)
+If you'd like to boot a different kernel or customise your commandline (to change the rootfs, for example) you can make a configuration file at `sd:/linux/boot.cfg`:
+```ini
+[loader]
+default=debian
+
+[profile:debian]
+name=Debian SID
+kernel=sdmc:/linux/dtbImage.wiiu
+cmdline=root=/dev/mmcblk0p2 rootwait
+```
+linux-loader's boot.cfg uses the concept of "profiles" to set up configurations for the Linux kernel. Note the section `[profile:debian]` - this creates a profile named `debian` with all the settings that follow.
+
+The first section, `[loader]`, defines settings to control linux-loader's behaviour. The only option it has right now is `default`, to set the profile to be booted. You can see in the example that this is set to `debian`, so linux-loader will find and use the settings under `[profile:debian]`. This option must be present to use the boot.cfg features. It's intended for forwards-compatibility in case we ever add a boot menu.
+
+The example then contains the section `[profile:debian]`, which defines a profile named `debian`. In this section are the `name`, `kernel` and `cmdline` options. You must define at least one profile in a boot.cfg. The `name` option sets a human-readable name or nickname for your profile. The next option, `kernel`, tells linux-loader where to load dtbImage.wiiu from, and can be any path. **Please note the use of `sdmc:/` instead of `sd:/`!** If this option is not present or the path cannot be read, linux-loader will fall back on `sdmc:/linux/dtbImage.wiiu` then `sdmc:/linux/kernel`. The final option, `cmdline`, sets the kernel commandline to any string. This is useful to control the behaviour of the kernel - in the example above, we set the rootfs to `/dev/mmcblk0p2` (the SD card [`mmcblk0`] partition 2 [`p2`]) allowing us to boot from the SD card! None of these options are strictly needed to use a boot.cfg - linux-loader has defaults it will fall back on, though it's recommended to at least have `name` and `cmdline`.
--- arm/application.c
+++ arm/application.c
@@ -21,6 +21,7 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 
 #include "system/smc.h"
@@ -33,17 +34,37 @@
 #include "common/utils.h"
 #include "storage/sd/sdcard.h"
 #include "storage/sd/fatfs/elm.h"
+#include "common/ini.h"
 
 #include "application.h"
 #include "ipc_protocol.h"
-
-#define LT_IPC_ARMCTRL_COMPAT_X1 0x4
-#define LT_IPC_ARMCTRL_COMPAT_Y1 0x1
 
 static const char* kernel_locs[] = {
 	"sdmc:/linux/dtbImage.wiiu",
 	"sdmc:/linux/kernel"
 };
+
+struct LoaderConfig {
+	char defaultProfile[64];
+};
+static struct LoaderConfig ldrConfig = { 0 };
+
+#define NUM_PROFILES 4
+struct ProfileConfig {
+	bool enabled;
+	char name[64];
+	char humanName[64];
+	char kernelPath[128];
+	char kernelCmd[256];
+};
+static struct ProfileConfig profiles[NUM_PROFILES] = { false };
+
+#define CMDLINE_CANARY_LOC 0x89200000
+#define CMDLINE_CANARY_MAGIC (unsigned int)0xCAFEFECA
+#define CMDLINE_LOC (char*)0x89200004
+
+#define LT_IPC_ARMCTRL_COMPAT_X1 0x4
+#define LT_IPC_ARMCTRL_COMPAT_Y1 0x1
 
 u32 SRAM_DATA ppc_entry = 0;
 
@@ -73,18 +94,90 @@ static void SRAM_TEXT NORETURN app_run_sram() {
 	}
 }
 
+static size_t search_for_profile(const char* name) {
+/*	Check if the profile exists */
+	for (size_t i = 0; i < NUM_PROFILES; i++) {
+		if (profiles[i].enabled) {
+			if (strcmp(name, profiles[i].name) == 0) {
+				return i;
+			}
+		}
+	}
+/*	If not, find the first unused slot and initialise it */
+	for (size_t i = 0; i < NUM_PROFILES; i++) {
+		if (!profiles[i].enabled) {
+			strncpy(profiles[i].name, name, sizeof(profiles[i].name));
+			profiles[i].enabled = true;
+			return i;
+		}
+	}
+	return ~0;
+}
+
+static int config_handler(void* user, const char* section, const char* name, const char* value) {
+	if (strcmp("loader", section) == 0) {
+		if (strcmp("default", name) == 0) {
+			strncpy(ldrConfig.defaultProfile, value, sizeof(ldrConfig.defaultProfile));
+		}
+	} else if (strncmp("profile:", section, 8) == 0) {
+		size_t ndx = search_for_profile(section + 8);
+		if (ndx == ~0) {
+			printf("Couldn't allocate profile for %s\n", section);
+			return 0;
+		}
+		if (strcmp("name", name) == 0) {
+			strncpy(profiles[ndx].humanName, value, sizeof(profiles[ndx].humanName));
+		} else if (strcmp("kernel", name) == 0) {
+			strncpy(profiles[ndx].kernelPath, value, sizeof(profiles[ndx].kernelPath));
+		} else if (strcmp("cmdline", name) == 0) {
+			strncpy(profiles[ndx].kernelCmd, value, sizeof(profiles[ndx].kernelCmd));
+		}
+	}
+	return 1;
+}
+
 void NORETURN app_run() {
 	int res;
 	bool kernel_loaded = false;
 
-	for (int i = 0; i < sizeof(kernel_locs) / sizeof(const char*); i++) {
-		printf("[INFO] Trying to load kernel from %s...\n", kernel_locs[i]);
-		res = ppc_load_file(kernel_locs[i], &ppc_entry);
-		if (res >= 0) {
-			kernel_loaded = true;
+/*	It doesn't really matter if this fails */
+	ini_parse("sdmc:/linux/boot.cfg", &config_handler, NULL);
+
+/*	Find the user-selected default profile */
+	size_t profileNdx;
+	bool profileFound = false;
+	for (profileNdx = 0; profileNdx < NUM_PROFILES; profileNdx++) {
+		if (!profiles[profileNdx].enabled) continue;
+		if (strcmp(ldrConfig.defaultProfile, profiles[profileNdx].name) == 0) {
+			profileFound = true;
 			break;
 		}
 	}
+
+/*	Load kernel according to config file profile */
+	if (profileFound) {
+		printf("[INFO] %d Trying to load kernel from %s...\n", profileNdx, profiles[profileNdx].kernelPath);
+		res = ppc_load_file(profiles[profileNdx].kernelPath, &ppc_entry);
+		if (res >= 0) kernel_loaded = true;
+
+	/*	Put kernel commandline at end of memory, ready for the boot wrapper to read */
+		if (strlen(profiles[profileNdx].kernelCmd) > 0) {
+			strncpy(CMDLINE_LOC, profiles[profileNdx].kernelCmd, sizeof(profiles[profileNdx].kernelCmd));
+			write32(CMDLINE_CANARY_LOC, CMDLINE_CANARY_MAGIC);
+		}
+	}
+
+/*	If that failed, use the default kernel locations */
+	if (!kernel_loaded) {
+		for (int i = 0; i < sizeof(kernel_locs) / sizeof(const char*); i++) {
+			printf("[INFO] Trying to load kernel from %s...\n", kernel_locs[i]);
+			res = ppc_load_file(kernel_locs[i], &ppc_entry);
+			if (res >= 0) {
+				kernel_loaded = true;
+				break;
+			}
+		}
+	}	
 
 	if (!kernel_loaded) {
 		printf("[FATL] Loading PowerPC kernel failed! (%d)\n", res);
